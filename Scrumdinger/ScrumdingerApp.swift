29a89fc4f0549c7b869d47ed857be08a5d85c144
lumana <luis.umanasalas@gmail.com>
2022-07-09 09:26:27 -0600

    Add project files

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Desarrollo SCOM on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
