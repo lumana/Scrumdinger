c5e6e48d8139386fd20423176f57248695267710
Luis <lumanas@bncr.fi.cr>
2022-07-10 11:35:31 -0600

    Passing data with bindings.

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
-enum Theme: String {
-
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -25,12 +24,13 @@ enum Theme: String {
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
-    
     var mainColor: Color {
         Color(rawValue)
     }
-    
     var name: String {
         rawValue.capitalized
+    }
+    var id: String {
+        name
     }
 }
